d7239baaff1448168cda2156bf938b74147f4e51
BillBrousalis <bill.brousalis@gmail.com>
2024-02-03 17:54:27 +0200

    finally a loop

--- run_tests.sh
+++ run_tests.sh
@@ -1,25 +1,7 @@
 #!/bin/bash
-echo "###################"
-echo "[*] Testing test1.c"
-SHOW_SOURCE=0 ./bcc tests/test1.c
-
-echo "###################"
-echo "[*] Testing test2.c"
-SHOW_SOURCE=0 ./bcc tests/test2.c
-
-echo "###################"
-echo "[*] Testing test3.c"
-SHOW_SOURCE=0 ./bcc tests/test3.c
-
-echo "###################"
-echo "[*] Testing test4.c"
-SHOW_SOURCE=0 ./bcc tests/test4.c
-
-echo "###################"
-echo "[*] Testing test5.c"
-SHOW_SOURCE=0 ./bcc tests/test5.c
-
-echo "###################"
-echo "[*] Testing test6.c"
-SHOW_SOURCE=0 ./bcc tests/test6.c
-
+for i in {1..8}
+do
+  echo "###################"
+  echo "[*] Testing test"$i".c"
+  SHOW_SOURCE=0 ./bcc tests/test"$i".c
+done
