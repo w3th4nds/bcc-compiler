d98f9264a70ec06be17dee3cd82ff7a0ef825135
BillBrousalis <bill.brousalis@gmail.com>
2024-02-06 00:47:56 +0200

    annotations on the graph

--- pyutil/plot.py
+++ pyutil/plot.py
@@ -7,14 +7,19 @@ import webbrowser
 def create_program_graph(fname):
   with open(fname, 'r') as f:
     lines = [l.strip() for l in f.readlines()]
-  # formatting
-  lines = [l.split(',')[:-1] for l in lines]
-  for il, line in enumerate(lines):
-    lines[il] = [n.replace(' ', '\n') for n in line]
-  tree = {line[0]:line[1:] for line in lines}
-  graph = Graph(tree, directed=True)
+
+  separator = lines.index('Annotations:')
+  nodes = [l.split(',')[:-1] for l in lines[:separator]]
+  for i, node in enumerate(nodes):
+    nodes[i] = [n.replace(' ', '\n') for n in node]
+  nodes = {node[0] : node[1:] for node in nodes}
+
+  annot = [l.replace(',', '').replace(' ', '\n').split('->') for l in lines[separator+1:]]
+  annot = {a[0]:a[1] for a in annot}
+
+  graph = Graph(nodes, directed=True, edges=annot)
   outname = f'{fname[:-4]}_graph.png'
-  graph.plot(output_path=outname, orientation='UD', fill_color='#BAC1D2')
+  graph.plot(output_path=outname, orientation='TB', fill_color='#BAC1D2')
   webbrowser.open(outname)
 
     
--- src/util.c
+++ src/util.c
@@ -47,13 +47,21 @@ char *format(AST_t *node)
   return buf;
 }
 
+char *format_annot(AST_t *paren, AST_t *child, char *annot)
+{
+  size_t sz = 1024;
+  char *buf = calloc(sz, sizeof(char));
+  snprintf(buf, sz, "%s%s->%s\n", format(paren), format(child), annot);
+  return buf;
+}
+
 // format AST in a way that can be used by plot.py
 void make_ast_graph(AST_t *root)
 {
   assert(root != NULL && "create_ast_file - root is NULL");
   List_t *list = init_list(sizeof(char *));   // data to write to file
   List_t *queue = init_list(sizeof(AST_t *)); // queue to iterate on
-  List_t *annot = init_list(sizeof(char *)); // TODO: build annotations for arrows
+  List_t *annot = init_list(sizeof(char *));  // TODO: build annotations for arrows
   list_push(queue, root);
   while (queue->size) {
     AST_t *node = (AST_t *)list_getitem(queue, 0);
@@ -69,7 +77,9 @@ void make_ast_graph(AST_t *root)
         break;
       case AST_BINOP:
         list_push(list, format(node->left));
+        list_push(annot, format_annot(node, node->left, "left"));
         list_push(list, format(node->right));
+        list_push(annot, format_annot(node, node->right, "right"));
         list_push(queue, node->left);
         list_push(queue, node->right);
         break;
@@ -77,28 +87,34 @@ void make_ast_graph(AST_t *root)
         // params
         for (int i = 0; i < node->children->size; ++i) {
           list_push(list, format(node->children->items[i]));
+          list_push(annot, format_annot(node, node->children->items[i], "param"));
           list_push(queue, node->children->items[i]);
         }
         // body
         list_push(list, format(node->body));
+        list_push(annot, format_annot(node, node->body, "body"));
         list_push(queue, node->body);
         break;
       case AST_COMPOUND:
         for (int i = 0; i < node->children->size; ++i) {
           list_push(list, format(node->children->items[i]));
+          list_push(annot, format_annot(node, node->children->items[i], "child"));
           list_push(queue, node->children->items[i]);
         }
         break;
       case AST_RETURN:
         list_push(list, format(node->value));
+        list_push(annot, format_annot(node, node->value, "value"));
         list_push(queue, node->value);
         break;
       case AST_ASSIGNMENT:
         if (node->decl != NULL) {
           list_push(list, format(node->decl));
+          list_push(annot, format_annot(node, node->decl, "decl"));
           list_push(queue, node->decl);
         }
         list_push(list, format(node->value));
+        list_push(annot, format_annot(node, node->value, "value"));
         list_push(queue, node->value);
         break;
       default: error_exit("create_ast_file() - unknown node type\n");
@@ -113,8 +129,16 @@ void make_ast_graph(AST_t *root)
     exit(1);
   }
 
+  // tree
   for (int i = 0; i < list->size; ++i)
     fwrite(list->items[i], sizeof(char), strlen(list->items[i]), fp);
+
+  // annotations
+  char *separator = "Annotations:\n";
+  fwrite(separator, sizeof(char), strlen(separator), fp);
+  for (int i = 0; i < annot->size; ++i)
+    fwrite(annot->items[i], sizeof(char), strlen(annot->items[i]), fp);
+  
   fclose(fp);
 
   // use python script
